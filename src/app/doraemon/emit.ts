c4dc893b3725a36c0aacabe7641b8f0b617e3879
sherotree <wytreeshero@163.com>
2024-01-10 22:17:19 +0800

    Add new files and dependencies

---
+++ src/app/doraemon/emit.ts
@@ -0,0 +1,9 @@
+export const emit = function (name: string, ...args: any): void {
+  window.parent.postMessage(
+    {
+      pluginMessage: [name, ...args],
+      pluginId: '1110011',
+    },
+    '*',
+  );
+};
