d510b2b7d9acdb10c4d7acceeb9dd942224ef31e
sherotree <wytreeshero@163.com>
2024-01-10 22:49:52 +0800

    Update pluginId in emit function

--- src/app/doraemon/emit.ts
+++ src/app/doraemon/emit.ts
@@ -2,7 +2,7 @@ export const emit = function (name: string, ...args: any): void {
   window.parent.postMessage(
     {
       pluginMessage: [name, ...args],
-      pluginId: '1110011',
+      pluginId: 'doraemon',
     },
     '*',
   );
