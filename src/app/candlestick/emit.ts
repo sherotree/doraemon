cc4f19299afc0e265713e203f8ca9be8c47e0721
sherotree <wytreeshero@163.com>
2024-01-13 13:35:45 +0800

    Add candlestick chart functionality

--- src/app/candlestick/emit.ts
+++ src/app/candlestick/emit.ts
@@ -2,7 +2,7 @@ export const emit = function (name: string, ...args: any): void {
   window.parent.postMessage(
     {
       pluginMessage: [name, ...args],
-      pluginId: 'doraemon',
+      pluginId: '1327873417658338157',
     },
     '*',
   );
